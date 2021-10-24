68d9619b24f45e3b56f5cb8ff1ae6d4ce9a6a04f
Ali Raza <alirazahumayun@uowdubai.ac.ae>
2021-10-24 11:14:17 +0400

    Initial Commit

---
+++ TestmyUOWD/TestmyUOWDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestmyUOWDApp.swift
+//  TestmyUOWD
+//
+//  Created by Ali on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestmyUOWDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
